0bf6190bde4b07dbdc6d9a6270ab3af64f37ae60
dlwl98 <ff981113@hanyang.ac.kr>
2023-01-25 20:03:18 +0900

    9663 N-Queen 정답 풀이

--- 4/9663.py
+++ 4/9663.py
@@ -1,22 +1,16 @@
 N = int(input())
 G = [[0] * N for _ in range(N)]
-can_put = [[True] * N for _ in range(N)]
+row = [0] * N
 result = 0
-dy = (-1, 1, 0, 0, -1, 1, -1, 1)
-dx = (0, 0, -1, 1, 1, 1, -1, -1)
 
 
-def change(i, j):
-    changes = [(i, j)]
-    can_put[i][j] = False
-    for k in range(N):
-        for n in range(8):
-            ni = i + dy[n] * k
-            nj = j + dx[n] * k
-            if 0 <= ni < N and 0 <= nj < N and can_put[ni][nj]:
-                changes.append((ni, nj))
-                can_put[ni][nj] = False
-    return changes
+def check(depth):
+    for i in range(depth):
+        if row[depth] == row[i]:
+            return False
+        if abs(row[depth] - row[i]) == depth - i:
+            return False
+    return True
 
 
 def dfs(depth):
@@ -25,11 +19,9 @@ def dfs(depth):
         result += 1
         return
     for x in range(N):
-        if can_put[depth][x]:
-            changes = change(depth, x)
+        row[depth] = x
+        if check(depth):
             dfs(depth + 1)
-            for a, b in changes:
-                can_put[a][b] = True
 
 
 def solution():
