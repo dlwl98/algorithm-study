93169af5f0602e18142008f6b778d01617a95ee8
dlwl98 <ff981113@hanyang.ac.kr>
2023-02-14 21:57:49 +0900

    2343 용돈 관리 정답

---
+++ 7/6236.py
@@ -0,0 +1,33 @@
+from sys import stdin
+
+N, M = map(int, input().split())
+G = []
+for _ in range(N):
+    G.append(int(stdin.readline().rstrip('\n')))
+
+
+def solution():
+    start = max(G)
+    end = sum(G)
+    while start <= end:
+        mid = (start + end) // 2
+
+        cnt = 0
+        tmp = 0
+        for i in range(N):
+            if tmp + G[i] > mid:
+                tmp = 0
+                cnt += 1
+            tmp += G[i]
+        if tmp:
+            cnt += 1
+
+        if cnt <= M:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    print(start)
+
+
+solution()
